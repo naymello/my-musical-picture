b175e7a856003849b02bbff7767bc9900a6f4451
naymello <naymellofilho@gmail.com>
2020-06-09 20:34:48 -0300

    Add settings page

---
+++ src/pages/settings.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import GlobalStyle from '../styles/GlobalStyle'
+
+import Form from '../components/Form'
+
+const SettingsPage = () => (
+  <>
+    <GlobalStyle />
+    <Form />
+  </>
+)
+
+export default SettingsPage
