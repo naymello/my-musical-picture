253e0ee2a01b44dd4ee2b934aa77c73c9ab69553
naymello <naymellofilho@gmail.com>
2020-06-10 22:41:44 -0300

    Replace default headers with styled components

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,13 +2,14 @@ import React from 'react'
 
 import StyledForm from '../styles/StyledForm'
 import Wrapper from '../styles/Wrapper'
+import { H2, H3 } from '../styles/Headers'
 
 const Form = () => {
   return (
     <Wrapper>
-      <h2>Settings</h2>
+      <H2>Settings</H2>
       <StyledForm method="GET" action="/picture">
-        <h3>What data to show:</h3>
+        <H3>What data to show:</H3>
         <input type="radio" id="artists" defaultValue="artists" name="type" defaultChecked="true" />
         <label htmlFor="artists">Artists</label>
         <input type="radio" id="albums" defaultValue="albums" name="type" />
@@ -16,7 +17,7 @@ const Form = () => {
         <input type="radio" id="tracks" defaultValue="tracks" name="type" />
         <label htmlFor="tracks">Songs</label>
 
-        <h3>Show data from:</h3>
+        <H3>Show data from:</H3>
         <input type="radio" id="short" defaultValue="short" name="time_range" defaultChecked="true" />
         <label htmlFor="short">Month</label>
         <input type="radio" id="medium" defaultValue="medium" name="time_range" />
@@ -24,7 +25,7 @@ const Form = () => {
         <input type="radio" id="long" defaultValue="long" name="time_range" />
         <label htmlFor="long">All time</label>
 
-        <h3>Picture theme:</h3>
+        <H3>Picture theme:</H3>
         <input type="radio" id="light" defaultValue="light" name="theme" defaultChecked="true" />
         <label htmlFor="light">Light</label>
         <input type="radio" id="dark" defaultValue="dark" name="theme" />
@@ -32,7 +33,7 @@ const Form = () => {
         <input type="radio" id="colored" defaultValue="colored" name="theme" />
         <label htmlFor="colored">Colored</label>
 
-        <h3>Show names:</h3>
+        <H3>Show names:</H3>
         <input type="radio" id="true" defaultValue="yes" name="captions" defaultChecked="true" />
         <label htmlFor="true">Yes</label>
         <input type="radio" id="false" defaultValue="no" name="captions" />
