7fa428c66c4798708ee3a55d6cf3e10536d5d6ec
naymello <naymellofilho@gmail.com>
2020-06-10 22:40:59 -0300

    Add Headers styled component

---
+++ src/styles/Headers.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const H1 = styled.h1`
+  font-size: 2rem;
+`
+
+export const H2 = styled.h2`
+  font-size: 1.8rem;
+`
+
+export const H3 = styled.h3`
+  font-size: 1.6rem;
+`
