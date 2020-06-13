ce1931baf7e131eeade3bd56e1d7be19db59f03a
naymello <naymellofilho@gmail.com>
2020-06-12 22:59:40 -0300

    Add styles to footer

---
+++ src/styles/StyledFooter.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const StyledFooter = styled.footer`
+  position: absolute;
+  bottom: 0;
+  display: flex;
+  width: 100%;
+  padding: 20px 0;
+  background-color: #F5F5F5;
+`
+
+export default StyledFooter
