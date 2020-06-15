478f9593de9a3e5976ac59fc11cedbadf61b5157
naymello <naymellofilho@gmail.com>
2020-06-15 14:14:39 -0300

    Add Button styled component

---
+++ src/styles/Button.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const Button = styled.button`
+  background-color: #EEFF00;
+  width: 100%;
+  height: 60px;
+  margin-top: auto;
+  color: #000;
+  border: none;
+  text-decoration: none;
+  text-transform: uppercase;
+  font-size: 2rem;
+  font-weight: 700;
+  cursor: pointer;
+`
+
+export default Button
