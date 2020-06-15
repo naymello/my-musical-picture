f0c2ea095d23a0c30f43b0e327e1b494f07674bc
naymello <naymellofilho@gmail.com>
2020-06-15 14:16:21 -0300

    Add StyledFooter styled component

--- src/styles/StyledFooter.js
+++ src/styles/StyledFooter.js
@@ -1,12 +1,16 @@
 import styled from 'styled-components'
 
 const StyledFooter = styled.footer`
-  position: absolute;
-  bottom: 0;
   display: flex;
   width: 100%;
   padding: 20px 0;
   background-color: #F5F5F5;
+  margin-top: 40px;
+
+  @media (min-width: 1024px) {
+    position: absolute;
+    bottom: 0;
+  }
 `
 
 export default StyledFooter
