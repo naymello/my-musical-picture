7c86b7b4303b2a92e6c5bea09432067c29c546b3
naymello <naymellofilho@gmail.com>
2020-06-15 13:51:25 -0300

    Decrease Wrapper width

--- src/styles/Wrapper.js
+++ src/styles/Wrapper.js
@@ -3,8 +3,11 @@ import styled from 'styled-components'
 import Navbar from './Navbar'
 import StyledFooter from './StyledFooter'
 
+/* This styled component is a wrapper used for centering the 
+   elements on the page when the viewport has a large width */
+
 const Wrapper = styled.div`
-  width: calc(100% - 20px);
+  width: calc(100% - 30px);
   max-width: ${props => props.maxWidth || '1440px'};
   margin: 100px auto 0;
 
