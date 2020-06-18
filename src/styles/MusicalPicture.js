d0d4d8db3b59cdb18189a56d9469810bfb946d75
naymello <naymellofilho@gmail.com>
2020-06-18 13:26:51 -0300

    Rename MusicalPicture styled component

--- src/styles/MusicalPicture.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-
-const MusicalPicture = styled.div`
-  
-`
-
-export default MusicalPicture
---
+++ src/styles/StyledPicture.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+const StyledPicture = styled.div`
+  
+`
+
+export default StyledPicture
