f8e58708396f6b6bb821216df902e11326b8ad90
naymello <naymellofilho@gmail.com>
2020-06-23 20:48:12 -0300

    Add style to StyledPicture styled component

--- src/styles/StyledPicture.js
+++ src/styles/StyledPicture.js
@@ -1,7 +1,27 @@
 import styled from 'styled-components'
 
 const StyledPicture = styled.div`
-  
+  background-color: #F4F4F4;
+  font-family: 'Poppins', sans-serif;
+  box-sizing: border-box;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 375px;
+  height: 667px;
+  padding: 5px 20px;
+
+  h3 {
+    font-size: 18px;
+  }
+
+  h4 {
+    margin-bottom: 10px;
+  }
+
+  span {
+    font-weight: 700;
+  }
 `
 
 export default StyledPicture
