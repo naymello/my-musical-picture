254c334c33a71b927f652df626f0d6bc680ff12f
naymello <naymellofilho@gmail.com>
2020-06-24 20:32:56 -0300

    Add Captions styled component

---
+++ src/styles/Captions.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+const Captions = styled.div`
+  background-color: ${props => props.theme.highlight};
+  color: ${props => props.theme.color};
+  position: relative;
+  top: 85%;
+  height: 15%;
+  width: 100%;
+  z-index: 1;
+  font-size: 10px;
+  font-weight: 600;
+  overflow: hidden;
+`
+
+export default Captions
