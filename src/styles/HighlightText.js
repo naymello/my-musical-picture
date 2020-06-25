0cbbaf34a78da0f8ebf56a0220180cb775ab7991
naymello <naymellofilho@gmail.com>
2020-06-25 19:23:41 -0300

    Add default color to HighlightText

--- src/styles/HighlightText.js
+++ src/styles/HighlightText.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 const HighlightText = styled.span`
-  background-color: ${props => props.theme.highlight};
+  background-color: ${props => props.theme.highlight || '#EEFF00'};
   padding: 0 7.5px;
 `
 
