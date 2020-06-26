3180f48026832c307950b21d25fc305f0fba70ad
naymello <naymellofilho@gmail.com>
2020-06-26 16:41:45 -0300

    Add custom style to Button after StyledPicture

--- src/styles/Button.js
+++ src/styles/Button.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components'
 
+import StyledPicture from '../styles/StyledPicture'
+
 const Button = styled.button`
   background-color: ${props => props.theme.highlight || '#EEFF00'};
   color: ${props => props.theme.color || '#000000'};
@@ -12,6 +14,11 @@ const Button = styled.button`
   font-size: 2rem;
   font-weight: 700;
   cursor: pointer;
+
+  ${StyledPicture} + & {
+    font-size: 1.6rem;
+    margin-top: 20px;
+  }
 `
 
 export default Button
