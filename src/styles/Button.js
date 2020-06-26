3cf276ae881c31df70707594e3890cd44ce9bc58
naymello <naymellofilho@gmail.com>
2020-06-26 15:07:29 -0300

    Add theming to Button

--- src/styles/Button.js
+++ src/styles/Button.js
@@ -1,11 +1,11 @@
 import styled from 'styled-components'
 
 const Button = styled.button`
-  background-color: #EEFF00;
+  background-color: ${props => props.theme.highlight || '#EEFF00'};
+  color: ${props => props.theme.color || '#000000'};
   width: 100%;
   height: 60px;
   margin-top: auto;
-  color: #000;
   border: none;
   text-decoration: none;
   text-transform: uppercase;
