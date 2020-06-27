b78cdc41e30751ad97e7598ccae03c9842ceabe9
naymello <naymellofilho@gmail.com>
2020-06-26 21:07:13 -0300

    Delete personal logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import Logo from './PersonalLogo'
-
 import StyledFooter from '../styles/StyledFooter'
 import Wrapper from '../styles/Wrapper'
 
@@ -9,8 +7,7 @@ const Footer = () => {
   return (
     <StyledFooter>
       <Wrapper>
-        <Logo fill="#000"/>
-        <h3 style={{color: '#000', margin: '0 20px'}}>Developed by Nay Mello</h3>
+        <h3 style={{color: '#000', margin: '0'}}>Developed by Nay Mello</h3>
       </Wrapper>
     </StyledFooter>
   )
