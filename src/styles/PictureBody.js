9912dfc1fc3026a8e751aab3b5c1e8c34c15bd2e
naymello <naymellofilho@gmail.com>
2020-07-01 15:29:48 -0300

    Fix picture visibility bug

--- src/styles/PictureBody.js
+++ src/styles/PictureBody.js
@@ -5,10 +5,10 @@ const PictureBody = createGlobalStyle`
     background-color: ${props => props.theme.bodyBackgroundColor};
     margin: 0;
     padding: 0;
+    display: flex;
+    justify-content: center;
 
-    @media (min-width: 375px) {
-      display: flex;
-      justify-content: center;
+    @media (min-height: 668px) {
       align-items: center;
       height: 100vh;
     }
